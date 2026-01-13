2fe4e7a029962850aba0efa6d90af5603ef126c3
Maximiliano Costa <maxcatiamilena2@gmail.com>
2026-01-12 23:32:18 -0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,6 +1,23 @@
 import streamlit as st
 import requests
 
+def calcular_imc(peso, altura):
+    if altura > 0:
+        imc = peso / (altura ** 2)
+        return round(imc, 2)
+    return 0
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 25:
+        return "Peso normal"
+    elif imc < 30:
+        return "Sobrepeso"
+    else:
+        return "Obesidade"
+
+
 # =========================
 # CONFIGURAÇÕES GERAIS
 # =========================
